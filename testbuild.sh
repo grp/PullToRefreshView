4b8d35a639155a2d968a5eecfef9f5275b97b037
Nathaniel Symer <nate@natesymer.com>
2013-03-29 21:47:40 -0700

    Removed outdated build script

--- testbuild.sh
+++
@@ -1,3 +0,0 @@
-# this will fail with _main undefined, but should work otherwise
-/Developer/Platforms/iPhoneOS.platform/Developer/usr/bin/clang++ -isysroot /Developer/Platforms/iPhoneOS.platform/Developer/SDKs/iPhoneOS5.0.sdk PullToRefreshView.m -arch armv7 -framework Foundation -framework UIKit -miphoneos-version-min=4.0 -framework QuartzCore
-
